a9e358a291d49a10cb0ffaeffaf01a58f96df1c2
Subhrajit Mandal <er.subhrajitm@gmail.com>
2025-09-02 17:08:10 +0000

    rstill showing wheatish background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'gradient-hero': 'linear-gradient(120deg, hsl(var(--primary)) 0%, hsl(var(--primary) / 0.8) 100%)',
+        'gradient-hero': 'linear-gradient(120deg, hsl(var(--secondary-blue)) 0%, hsl(var(--primary)) 100%)',
         grain: "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 700 700' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.6' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E\")",
       },
       fontFamily: {
